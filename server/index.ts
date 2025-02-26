1806e0128d1233cc4054c80cc7250809eb245271
gsubhampatra <gpb021@nist.edu>
2025-02-26 16:23:20 +0530

    cors

--- server/index.ts
+++ server/index.ts
@@ -1,21 +1,25 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import routes from './routes/routes';
-import { corsOptions } from './lib/cors/cors';
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import routes from "./routes/routes";
+import { corsOptions } from "./lib/cors/cors";
 
 const app = express();
-app.use(cors(corsOptions));
-// app.use(cors());
+// app.use(cors(corsOptions));
+app.use(
+  cors({
+    credentials: true,
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api", routes);
 
-
-app.get('/health', (req, res) => {
-    return res.json({ msg: 'Server is running' });
-})
+app.get("/health", (req, res) => {
+  return res.json({ msg: "Server is running" });
+});
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('Server running on port 3000');
+  console.log("Server running on port 3000");
 });
